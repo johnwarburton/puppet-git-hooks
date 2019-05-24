2b5467a9a8691749f37505f3fbcdea6598085db5
John Warburton <john.warburton@epochcapital.com.au>
2019-05-24 10:08:50 +1000

    ITOP-2393 r10k no longer supports  PUPPETFILE environment variable. Use local fork

--- commit_hooks/r10k_syntax_check.sh
+++ commit_hooks/r10k_syntax_check.sh
@@ -3,7 +3,9 @@
 # This script assumes you have installed r10k and will perform a syntax check on the Puppetfile if existing
 
 echo "Performing a syntax check on the r10k Puppetfile:"
-PUPPETFILE="$1" r10k puppetfile check
+pushd `dirname $1`
+r10k puppetfile check
+popd
 
 if [[ $? -ne 0 ]]
 then
